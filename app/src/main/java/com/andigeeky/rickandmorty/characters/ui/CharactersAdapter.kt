06222e23402dd65d199286e36fba67df07a096bb
Mamata <gelaneem@gmail.com>
2021-04-03 12:43:06 +0100

    [ADD] character scroll with pagination (#6)
    * [ADD] character scroll with pagination
    
    * [FIX] next page can be null, we can not assume null = 0

--- app/src/main/java/com/andigeeky/rickandmorty/characters/ui/CharactersAdapter.kt
+++ app/src/main/java/com/andigeeky/rickandmorty/characters/ui/CharactersAdapter.kt
@@ -9,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.andigeeky.rickandmorty.R
 import com.andigeeky.rickandmorty.characters.model.Character
+import com.andigeeky.rickandmorty.characters.model.Gender
 
 class CharactersAdapter : RecyclerView.Adapter<CharactersAdapter.CharacterViewHolder>() {
     private val characters = mutableListOf<Character>()
@@ -22,8 +23,23 @@ class CharactersAdapter : RecyclerView.Adapter<CharactersAdapter.CharacterViewHo
         characters[position].url?.let {
             holder.image.load(it)
         }
-        holder.title.text = characters[position].name.orEmpty()
-        holder.subtitle.text = characters[position].gender.gender
+        holder.title.text = holder.title.context.getString(
+            R.string.characters_title_character_name,
+            characters[position].id?.id.orEmpty(),
+            characters[position].name.orEmpty()
+        )
+        holder.subtitle.text = holder.subtitle.context.getString(
+            R.string.characters_subtitle_no_of_episodes,
+            characters[position].episodes.size
+        )
+        holder.imageGender.setImageResource(
+            when(characters[position].gender){
+                Gender.FEMALE -> R.drawable.ic_female_black_24dp
+                Gender.MALE -> R.drawable.ic_male_black_24dp
+                Gender.GENDERLESS -> R.drawable.ic_transgender_black_24dp
+                Gender.UNKNOWN -> R.drawable.ic_transgender_black_24dp
+            }
+        )
     }
 
     override fun getItemCount() = characters.size
@@ -36,6 +52,7 @@ class CharactersAdapter : RecyclerView.Adapter<CharactersAdapter.CharacterViewHo
 
     class CharacterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var image: ImageView = view.findViewById(R.id.item_image_character)
+        var imageGender: ImageView = view.findViewById(R.id.item_image_character_gender)
         var title: TextView = view.findViewById(R.id.item_title_character)
         var subtitle: TextView = view.findViewById(R.id.item_subtitle_character)
     }
--- app/src/main/java/com/andigeeky/rickandmorty/characters/ui/CharactersEvent.kt
+++ app/src/main/java/com/andigeeky/rickandmorty/characters/ui/CharactersEvent.kt
@@ -6,5 +6,8 @@ import io.uniflow.core.flow.data.UIEvent
 sealed class CharactersEvent : UIEvent(){
     object ShowLoadingCharacters : CharactersEvent()
     object HideLoadingCharacters : CharactersEvent()
+    object ShowLoadingNextPageCharacters : CharactersEvent()
+    object HideLoadingNextPageCharacters : CharactersEvent()
     data class ShowPersistentError(@StringRes val message : Int) : CharactersEvent()
+    data class LoadNextCharactersPage(val page : Int?) : CharactersEvent()
 }
